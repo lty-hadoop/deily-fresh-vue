c611d8e8076e2401fa216f8c05176e14be3c2174
huyalong <sp123456>
2019-05-29 15:39:08 +0800

    冒泡排序

--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -35,5 +35,26 @@ export default {
 	getUserInfo () {
 		let userObj = JSON.parse(localStorage.getItem('user'))
 		return userObj
+	},
+
+
+	// 冒泡排序，，排json
+	bubbleSort (arr, type, direction) {
+		let len = arr.length;
+		for (let i = 0; i < len - 1; i++) {
+	        for(let j = 0; j < len - 1 - i;j ++) {
+	            if (direction == 'up') {
+					if(arr[j][type] > arr[j+1][type]) {
+	                     [arr[j+1],arr[j]] = [arr[j],arr[j+1]]
+
+	                }
+				} else {
+					if(arr[j][type] < arr[j+1][type]) {
+	                     [arr[j+1],arr[j]] = [arr[j],arr[j+1]]
+	                }
+				}
+	        }
+	    }
+		return arr
 	}
 }
